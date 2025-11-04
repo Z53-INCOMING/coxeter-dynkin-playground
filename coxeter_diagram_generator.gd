fc758c328a7bc04b53529ae6fb57fa3f70cce2d5
Tessimal <75651517+Z53-INCOMING@users.noreply.github.com>
2025-11-04 02:25:08 -0800

    node spawning

--- coxeter_diagram_generator.gd
+++ coxeter_diagram_generator.gd
@@ -6,6 +6,8 @@ var last_spawn_position := Vector2(-1296.0, 0.0)
 
 var last_node: CoxeterNode = null
 
+@onready var coxeter_node_scene := preload("res://coxeter_node.tscn")
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT:
@@ -14,8 +16,22 @@ func _input(event):
 
 func _process(delta):
 	if drawing:
-		if last_spawn_position.distance_squared_to(get_global_mouse_position()) > 180.0 * 180.0:
+		if last_spawn_position.distance_squared_to(get_global_mouse_position()) > 120.0 * 120.0:
 			spawn_node(last_node)
 
 func spawn_node(connect_to: CoxeterNode):
-	pass
+	var coxeter_node: CoxeterNode = coxeter_node_scene.instantiate()
+	
+	coxeter_node.position = get_global_mouse_position()
+	coxeter_node.linear_velocity = random_point_in_circle(18.0)
+	last_spawn_position = get_global_mouse_position()
+	
+	add_child(coxeter_node)
+
+func random_point_in_circle(radius: float) -> Vector2:
+	var point := Vector2(randf_range(-radius, radius), randf_range(-radius, radius))
+	
+	while point.length_squared() > radius * radius:
+		point = Vector2(randf_range(-radius, radius), randf_range(-radius, radius))
+	
+	return point
--- coxeter_node.gd
+++ coxeter_node.gd
@@ -2,6 +2,16 @@ class_name CoxeterNode extends RigidBody2D
 
 @onready var circle := $Circle
 @onready var ring := $Ring
+@onready var hitbox := $Hitbox
+
+@onready var ringed_node_shape := preload("res://ringed_node.tres")
+@onready var unringed_node_shape := preload("res://unringed_node.tres")
+
+var ringed: bool = false :
+	set(value):
+		ringed = value
+		ring.visible = value
+		hitbox.shape = ringed_node_shape if ringed else unringed_node_shape
 
 func _ready():
 	var angle := 0.0
@@ -14,3 +24,5 @@ func _ready():
 	for i in 63:
 		points[i] *= 1.56
 	ring.points = points
+	
+	ringed = randi() % 2 == 0
