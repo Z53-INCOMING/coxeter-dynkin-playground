3bf82984dc1b1727d3043af4f8ef31f61692adf6
Tessimal <75651517+Z53-INCOMING@users.noreply.github.com>
2025-11-04 18:56:01 -0800

    toggleable gravity

--- coxeter_diagram_generator.gd
+++ coxeter_diagram_generator.gd
@@ -10,12 +10,26 @@ var second_to_last_node: CoxeterNode = null
 @onready var coxeter_node_scene := preload("res://coxeter_node.tscn")
 @onready var coxeter_edge_scene := preload("res://coxeter_edge.tscn")
 
+var has_gravity := false
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			drawing = !drawing
 			last_node = null
 			second_to_last_node = null
+	if event is InputEventKey:
+		if event.keycode == KEY_DOWN and event.pressed:
+			has_gravity = !has_gravity
+			
+			if has_gravity:
+				var physics_space := get_viewport().find_world_2d().space
+				PhysicsServer2D.area_set_param(physics_space, PhysicsServer2D.AREA_PARAM_GRAVITY, 2592.0)
+				PhysicsServer2D.area_set_param(physics_space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
+			else:
+				var physics_space := get_viewport().find_world_2d().space
+				PhysicsServer2D.area_set_param(physics_space, PhysicsServer2D.AREA_PARAM_GRAVITY, 0.0)
+				PhysicsServer2D.area_set_param(physics_space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.ZERO)
 
 func _process(_delta):
 	if drawing:
